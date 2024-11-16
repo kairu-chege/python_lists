2e16a571d1beb7ec73ba5f77b2bea24ab4d6d2f8
Kairu <127830981+kairu-chege@users.noreply.github.com>
2024-11-16 15:04:52 +0300

    Update python_lists.py
    Commenting the steps for easier code understanding

--- python_lists.py
+++ python_lists.py
@@ -1,4 +1,4 @@
-# initialize an empty list 
+# Step 1: initialize an empty list 
 my_list = []
 # appending the empty list 
 my_list.append(10)
@@ -8,26 +8,26 @@ my_list.append(40)
 
 print(my_list)
 
-# inserting 15 at the second position in the list 
+# Step 2: inserting 15 at the second position in the list 
 new_element = 15
 my_list.insert(2, new_element)
 
 print(my_list)
 
-# extending my_list
+# Step 3: extending my_list
 my_list2 = [50, 60, 70]
 my_list.extend(my_list2)
 
 print(my_list)
 
-# removing the last element from my_list
+# Step 4: removing the last element from my_list
 my_list.pop()
 print(my_list)
 
-#sorting my_list in ascending order usig the sorted() function
+# Step 5: sorting my_list in ascending order usig the sorted() function
 sorted_list = sorted(my_list)
 print(sorted_list)
 
-#findinf and printing the index of the value 30
+# Step 6: finding and printing the index of the value 30
 index_of_30 = my_list.index(30)
 print(f"The index of the value 30 is: {index_of_30}")
